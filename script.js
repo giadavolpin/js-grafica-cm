ce4eae1c1a1644160c3a496dd3d1fa40d52b155c
giadavolpin <volpingiada@gmail.com>
2022-10-17 16:38:17 +0200

    html css

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; */
+
+
+
+
