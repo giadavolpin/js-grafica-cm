274042e95d10e0977c0624f6f777ad84b6ca2362
giadavolpin <volpingiada@gmail.com>
2022-10-18 18:25:58 +0200

    griglia

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 /* L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio 
+in console con il numero della cella cliccata.
+
 Bonus
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
@@ -12,45 +14,39 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 
 //richiamo il bottone dall'HTML
 const playButton = document.getElementById('playButton');
+let numeroCell = 100;
 
+//creo la funzione 
 function play (){
     console.log('inizio gioco...')
-    const numeroCell = 100;
-    
-    function Celle(numeroCell){
-        const cell = document.createElement('div');
-        cell.className = 'square';
-        cell.innerHTML = `
-        <span>${numeroCell}<span>
-        `;
-        return cell;
-    }
-
-    // al click del bottone si genererà la griglia di gioco. 
-    function campoMinato(){
-        const campoGioco = document.getElementById('campoDiGioco'); 
-        const grid = document.createElement('div');
-        grid.className = 'grid' //griglia
-        for (let i= 1; i<= 100; i++){
-            const cella =  numeroCell(i);
-            grid.appendChild(cell);
-        }
-        campoGioco.appendChild(grid);
-    }
-   
+    campoMinato(); 
 }
- 
-//chiamo la funzione fuori dalla funzione stessa
+//creo le celle 
+function creaCelle(numero){
+    const cell = document.createElement('div');
+    cell.className = 'square';
+    cell.innerHTML = `
+    <span>${numero}<span>
+    `;
+    return cell;
+}
+// creo la griglia di gioco. 
+function campoMinato(){
+    const campoGioco = document.getElementById('campoDiGioco'); 
+    const grid = document.createElement('div');
+    grid.className = 'grid' //griglia
+    for (let i= 1; i<= numeroCell; i++){
+        grid.appendChild(creaCelle(i));  //campominato chiama crea celle 100 volte. 
+    }
+    campoGioco.appendChild(grid);
+    
+}
+//chiamo la funzione iniziale
 play();
 
+
 // L'utente clicca su un bottone 
-playButton.addEventListener('click', playButton);
-
-
-   
+playButton.addEventListener('click', play());
 
 
 
-
-
-// Ogni cella ha un numero progressivo, da 1 a 100.
